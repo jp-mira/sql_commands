6a1593a831274388cc63c23a6b08ed6b81f7368b
Jerome Paulo Mira <jeromepaulomira@gmail.com>
2026-03-23 13:45:03 +0800

    This is the my first commit in this SQL commands

---
+++ SQL_Commands.sql
@@ -0,0 +1,7 @@
+-- Selecting parks and recreation database and then the employee demographics
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+-- Selecting the first name from the employee demographics
+SELECT first_name
+FROM parks_and_recreation.employee_demographics;
