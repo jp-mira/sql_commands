66c6c3a0c07573932bd68d2bad6eab69d6b3d219
Jerome Paulo Mira <jeromepaulomira@gmail.com>
2026-03-23 13:57:18 +0800

    Adding the selection of table from the database and display

--- SQL_Commands.sql
+++ SQL_Commands.sql
@@ -5,3 +5,7 @@ FROM parks_and_recreation.employee_demographics;
 -- Selecting the first name from the employee demographics
 SELECT first_name
 FROM parks_and_recreation.employee_demographics;
+
+-- Separating the first name, last name, and birthdate then showing as table
+SELECT first_name, last_name, birth_date
+FROM parks_and_recreation.employee_demographics;
