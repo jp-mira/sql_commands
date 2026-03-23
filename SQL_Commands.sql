e998b1cff1a147427683ea589dd1b42d7f09de8b
Jerome Paulo Mira <jeromepaulomira@gmail.com>
2026-03-23 14:12:58 +0800

    This is will be simple arithmetic equations in SQL

--- SQL_Commands.sql
+++ SQL_Commands.sql
@@ -9,3 +9,7 @@ FROM parks_and_recreation.employee_demographics;
 -- Separating the first name, last name, and birthdate then showing as table
 SELECT first_name, last_name, birth_date
 FROM parks_and_recreation.employee_demographics;
+
+-- Making cimple calculation in SQL which follows the PEMDAS rule
+SELECT age, age+10
+FROM parks_and_recreation.employee_demographics;
